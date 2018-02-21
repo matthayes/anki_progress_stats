d158e3ab6c0b683b5606cf462f370dcb35e21001
Matthew Hayes <matthew.terence.hayes@gmail.com>
2018-02-20 21:13:13 -0800

    Add test and CI scaffolding

---
+++ Makefile
@@ -0,0 +1,9 @@
+init:
+	pip install pipenv --upgrade
+	pipenv install --dev --skip-lock
+
+test:
+	pipenv run py.test
+
+flake8:
+	pipenv run flake8 --ignore=E501
---
+++ tests/test_basic.py
@@ -0,0 +1,8 @@
+from progress_stats.compute import ProgressStats
+
+
+class TestBasic:
+
+    # silly test just to make sure we use ProgressStats
+    def test_basic(self):
+        ProgressStats()
